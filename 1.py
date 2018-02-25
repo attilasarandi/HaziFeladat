ee2b5491fe7bbebd361d38a903fd5c01700dbdef
attilasarandi <36511608+attilasarandi@users.noreply.github.com>
2018-02-25 12:46:43 +0200

    Add files via upload

--- 1.py
+++ 1.py
@@ -229,14 +229,13 @@ def feladat_20_1(n):
             print(z,end=" ")
             k=k+1
 def feladat_20_2(n):
-    x=1
-    y=1
+    a=1
+    b=1
     k=1
-    print()
     while k<=n:
-        print(y,end=" ")
-        x=x+y
-        y=x-y
+        print(b,end=" ")
+        a=a+b
+        b=a-b
         k=k+1
 def feladat_21(n):
     ujszam=0
@@ -254,6 +253,86 @@ def feladat_22(alap,kitevo):
         alap=alap*alap
         kitevo=kitevo//2
     return eredmeny
+
+def feladat_23(n):
+    a=0
+    for i in range(1, n):
+        if n%i == 0:
+            a=a+i
+    return a==n
+
+def feladat_24():
+    a=0
+    b=0
+    while True:
+        c=int(input("input: "))
+        if c==0:
+            break
+        elif c%7==5:
+            a=a+1
+        elif c%13==7:
+            b=b+1
+    print("%d %d"%(a,b))
+
+def feladat_25():
+    fo=int(input("fo: "))
+    km2=int(input("km: "))
+    n=fo//km2
+    if n>0 and n<50:
+            print("Ritkan lakott nepsuruseg")
+    elif n>50 and n<300:
+            print("Atlagos nepsuruseg")
+    elif n>300:
+            print("Surun lakott")
+    else:
+        print("Hibás adatok")
+
+def feladat_26():
+    osszeg=0
+    neg=0
+    poz=0
+    while True:
+        a=int(input("szam: "))
+        if a==0:
+            break
+        else:
+            osszeg=osszeg+a
+            print(osszeg)
+            if a>0:
+                poz=poz+1
+            elif a<0:
+                neg=neg+1
+    print("pozitiv szamok: ",poz)
+    print("negativ szamok: ",neg)
+
+def feladat_27():
+    poz=1
+    db=0
+    neg=1
+    db1=0
+    while True:
+        szam=int(input("szam: "))
+        if szam>0:
+            poz=poz+1
+            db=db+1
+        elif szam<0:
+            neg+=1
+            db1=db1+1
+        if abs(poz-neg)>1:
+            break
+    print("Pozitiv szamok:",db)
+    print("Negativ szamok:",db1)
+def feladat_28(n):
+    for i in range(1,n+1):
+        if i*i<n:
+            print(i,end=" ")
+
+def feladat_29(szam):
+    fakt= 1
+    while szam >= 1 and szam<=11:
+        fakt=fakt*szam
+        szam=szam- 1
+    return fakt
 def main():
     feladat_1(5,2)
     feladat_2()
@@ -261,23 +340,31 @@ def main():
     feladat_4(-1,-1,-1)
     feladat_5(1,-3,-2,5)
     feladat_6()
-     print(feladat_7(2,3,8))
-    feladat_8(4,1,-2,3,4)
-    print(feladat_9(3,4,1))
-    feladat_10(1900,2020)
-    feladat_12(60,100)
+    print(feladat_7(2, 3, 8))
+    feladat_8(4, 1, -2, 3, 4)
+    print(feladat_9(3, 4, 1))
+    feladat_10(1900, 2020)
+    feladat_12(60, 100)
     feladat_13(3)
     feladat_14(4)
-    print(feladat_15(15,5))
-    print(feladat_16(360,225))
+    print(feladat_15(15, 5))
+    print(feladat_16(360, 225))
     print(feladat_17(11))
-    print(feladat_18(45,17))
+    print(feladat_18(45, 17))
     print(feladat_19(11))
     feladat_20_1(5)
     print()
     feladat_20_2(5)
     print()
     print(feladat_21(135))
-    print(feladat_22(3,4))
+    print(feladat_22(3, 4))
+    print(feladat_23(6))
+    feladat_24()
+    feladat_25()
+    feladat_26()
+    feladat_27()
+    feladat_28(26)
+    print()
+    print(feladat_29(5))
 if __name__ == '__main__':
     main()
